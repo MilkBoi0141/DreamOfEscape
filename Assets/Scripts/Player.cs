d9976269cb95f0fae2c9367c7a1d34b2733c5522
GitAsh-o <rrma.aah@gmail.com>
2024-02-27 01:24:55 +0900

    Add Method MovePlayer()

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float speed = 5.0f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(x, 0, z);
     }
 }
