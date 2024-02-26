635206bb6a3ccd756bfc22dc3de5ff288524baf7
GitAsh-o <rrma.aah@gmail.com>
2024-02-27 04:19:09 +0900

    Add stage and player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
-using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float speed = 5.0f;
+    [SerializeField]private float _speed = 5.0f;
+    [SerializeField]private bool _canJump = false;
 
     // Start is called before the first frame update
-    private void Start()
+    private async void Start()
     {
-
+        while(true)
+        {
+            await UniTask.WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+            if (_canJump == true)
+            {
+                transform.position += new Vector3(0, 50, 0);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -23,9 +30,15 @@ public class Player : MonoBehaviour
 
     private void MovePlayer()
     {
-        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float x = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
 
         transform.Translate(x, 0, z);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        CheckFloor checkFloor = other.gameObject.GetComponent<CheckFloor>();
+        _canJump = checkFloor.existStage;
     }
 }
