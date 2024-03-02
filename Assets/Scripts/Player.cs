71aeb4539fc69b102f949f56fdcc4f4602a7f729
GitAsh-o <rrma.aah@gmail.com>
2024-03-03 01:36:04 +0900

    Improve collision of goal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     [SerializeField]private GameObject mainCmaera;
     [SerializeField]private Canvas mapCanvas;
     [SerializeField]private Vector3 prevPos;
+    [SerializeField]private Vector3 delta;
     [SerializeField]private float speed = 5.0f;
     [SerializeField]private int floorVal = 1;
     [SerializeField]private bool canJump = false;
@@ -27,10 +28,12 @@ public class Player : MonoBehaviour
                 {
                     transform.position += new Vector3(0, 50, 0);
                     floorVal = 2;
+                    prevPos += new Vector3(0, 50, 0);
                 }else if(floorVal == 2)
                 {
                     transform.position += new Vector3(0, -50, 0);
                     floorVal = 1;
+                    prevPos += new Vector3(0, -50, 0);
                 }
             }
         }
@@ -64,7 +67,7 @@ public class Player : MonoBehaviour
     //プレイヤーの回転を実装
     public void RotatePlayer()
     {
-        var delta = transform.position - prevPos;
+        delta = transform.position - prevPos;
         prevPos = transform.position;
         if (delta == Vector3.zero)
         {
@@ -75,11 +78,22 @@ public class Player : MonoBehaviour
 
     private void OnTriggerStay(Collider _other)
     {
-        //ジャンプの可能不可能に関する実装
-        CheckFloor checkFloor = _other.gameObject.GetComponent<CheckFloor>();
-        canJump = checkFloor.existOtherStage;
+        if (_other.gameObject.CompareTag("Stage"))
+        {
+            //ジャンプの可能不可能に関する実装
+            CheckFloor checkFloor = _other.gameObject.GetComponent<CheckFloor>();
+            canJump = checkFloor.existOtherStage;
 
-        //カメラ遷移に関する実装
-        mainCmaera.transform.position = _other.gameObject.transform.position + new Vector3(0, 20, -8);
+            //カメラ遷移に関する実装
+            mainCmaera.transform.position = _other.gameObject.transform.position + new Vector3(0, 20, -8);
+        }
+    }
+
+    private void OnTriggerEnter(Collider _other)
+    {
+        if (_other.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("Goal");
+        }
     }
 }
